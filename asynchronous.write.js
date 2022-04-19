d5a55bfdc20fd9f62e614c90d3862c57797dfff3
Darchele Webb <Darchele.Webb@rgare.com>
2022-04-19 11:11:13 -0500

    write with options

--- asynchronous.write.js
+++ asynchronous.write.js
@@ -1,9 +1,9 @@
-const { writeFile } = require('fs')
+const { constants, writeFile } = require('fs')
 
 writeFile(
-	'./data/app.log',
+	'./data/newapp.log',
 	'163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512',
-	{ flag: 'wx' },
+	{ encoding: 'base64' },
 	(err) => {
 		err ? console.log(err) : console.log('file saved!!')
 	},
